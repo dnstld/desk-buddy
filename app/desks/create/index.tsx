43e2d56f3cab71fca9d194c8b060fd5e8e637ced
Denis Toledo <detoledo.denis@gmail.com>
2025-10-07 14:29:13 +0200

    chore: rename desks route (delete)

--- app/desks/create/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function CreateDesk() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Create New Desk</Text>
-    </View>
-  );
-}
