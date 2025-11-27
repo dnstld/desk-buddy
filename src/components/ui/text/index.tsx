3132b4403488030a1560a9d85b4e4306a1d4f3d4
Denis Toledo <detoledo.denis@gmail.com>
2025-11-27 16:54:11 +0100

    create Text component

---
+++ src/components/ui/text/index.tsx
@@ -0,0 +1,25 @@
+import { clsx } from "clsx";
+import { Text as RNText, TextProps as RNTextProps } from "react-native";
+
+interface TextProps extends RNTextProps {
+  variant?: "xs" | "sm" | "base" | "lg" | "xl" | "2xl";
+}
+
+export default function Text({
+  className,
+  variant = "base",
+  ...props
+}: TextProps) {
+  const variantClasses = {
+    xs: "text-xs",
+    sm: "text-sm",
+    base: "text-base",
+    lg: "text-lg",
+    xl: "text-xl",
+    "2xl": "text-2xl",
+  };
+
+  return (
+    <RNText className={clsx(variantClasses[variant], className)} {...props} />
+  );
+}
